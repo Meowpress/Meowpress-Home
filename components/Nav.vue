c908d1331e44d971c22365bb0b690805aa49a742
spencerwooo <spencerwoo98@foxmail.com>
2018-11-14 23:10:22 +0800

    🤳 First page complete!

---
+++ components/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Navbar">
+    <b-navbar toggleable="md" type="light" variant="faded" fixed="top">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand tag="h1" class="mb-0">Meowpress</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+      <b-nav-form>
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Email"/>
+        <b-button size="sm" class="submit-email" variant="dark">Sign in</b-button>
+      </b-nav-form>
+
+      <b-nav-item-dropdown text="Translations" right>
+        <b-dropdown-item href="#">English</b-dropdown-item>
+        <b-dropdown-item href="#">简体中文</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown right>
+        <!-- Using button-content slot -->
+        <template slot="button-content">
+          User
+        </template>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <b-dropdown-item href="#">Sign out</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+    </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style>
+.Meowpress-nav-logo {
+  margin-right: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
+<div>
+  <Nav/>
   <section class="container">
     <div>
-      <logo/>
+      <Logo/>
       <h1 class="title">
         Meowpress
       </h1>
@@ -17,29 +19,52 @@
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey">GitHub</a> -->
-        <b-button href="#" size="lg" variant="primary">
-          即刻开始 →
-        </b-button>
-        <b-button href="https://github.com/nuxt/nuxt.js" size="lg" variant="primary">
-          GitHub
+        <b-button href="#" size="lg" variant="dark">
+          <div class="main-button-text">
+            建站指北
+          </div>
         </b-button>
       </div>
     </div>
   </section>
+  <section class="intro-container-1">
+    <h2>
+      顺滑、Markdown 友好的博客体验
+    </h2>
+  </section>
+  <section class="intro-container-2">
+    <h2>
+      精致、编辑精选的免费主题
+    </h2>
+  </section>
+  <section class="intro-container-3">
+    <h2>
+      五分钟迅捷建站
+    </h2>
+  </section>
+  <div class="footer">
+    Follow us on
+    <a href="#foo"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a> · 
+    <a href="#foo"><i class="fa fa-telegram" aria-hidden="true"></i> Telegram</a> · 
+    <a href="#foo"><i class="fa fa-weibo" aria-hidden="true"></i> Weibo</a> and more.<br>
+    ©2018 Proudly made with <i class="fa fa-heart" aria-hidden="true"></i> from BIT.
+  </div>
+</div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Nav from '~/components/Nav.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Nav
   }
 }
 </script>
 
 <style>
-
 .container {
   min-height: 100vh;
   display: flex;
@@ -52,7 +77,7 @@ export default {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 600;
+  font-weight: 300;
   font-size: 64px;
   color: #2c2c2c;
   letter-spacing: 1px;
@@ -68,5 +93,40 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.main-button-text {
+  padding: 6px 0;
+}
+
+.intro-container-1, .intro-container-2, .intro-container-3 {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.intro-container-1, .intro-container-3 {
+  background-color: #FAFAFA;
+}
+
+.footer {
+  padding: 3rem;
+  text-align: center;
+  color: #fff;
+  text-decoration: none;
+
+  background-color: #343A40;
+  line-height: 2rem;
+}
+
+a {
+  text-decoration: none;
+  color: #FAFAFA;
+}
+
+a:hover {
+  color: #FAFAFA;
 }
 </style>
